593f36f0b4ebdcb7b591c39c29ccdab18d6b13f2
Metrowy <bari480@gmail.com>
2019-01-12 02:05:01 +0100

    Add template to project and create first controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+    class MainController extends AbstractController
+    {
+        /**
+         * @Route("/")
+         */
+        public function index()
+        {
+            return $this->render('main/index.html.twig');
+        }
+    }
